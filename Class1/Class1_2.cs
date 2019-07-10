af56487de8c670275544bb634064c07588050f3d
AlvinDuo <king870110@gmail.com>
2019-07-10 11:00:19 +0800

    加入專案檔案。

---
+++ Class1/Class1_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DaHua.Class1
+{
+    class Class1_2
+    {
+        //相對於1-1是更好的版本
+        
+    }
+}
---
+++ Class1/class1_1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DaHua
+{
+    class class1_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入第1個數字A:");
+            string a = Console.ReadLine();
+            Console.WriteLine("輸入+.-.*./");
+            string operate = Console.ReadLine();
+            Console.WriteLine("輸入第2個數字B:");
+            string b = Console.ReadLine();
+            if (operate == "+")
+            {
+                Console.WriteLine(Convert.ToDouble(a) + Convert.ToDouble(b));
+            }
+            else if (operate == "-")
+            {
+                Console.WriteLine(Convert.ToDouble(a) - Convert.ToDouble(b));
+            }
+            else if (operate == "*")
+            {
+                Console.WriteLine(Convert.ToDouble(a) * Convert.ToDouble(b));
+            }
+            else if (operate == "/")
+            {
+                if (Convert.ToDouble(b) != 0)
+                {
+                    Console.WriteLine(Convert.ToDouble(a) / Convert.ToDouble(b));
+                }
+                else
+                {
+                    Console.WriteLine("B不能為0");
+                }
+            }
+            else
+            {
+                Console.WriteLine("輸入有誤");
+            }
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
